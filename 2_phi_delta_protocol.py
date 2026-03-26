ed36df083874019a1574550ab4cc766fbb5e7d86
JaimeQLZ <Comunidadcristiana3333@proton.me>
2026-03-26 16:19:10 +0100

    Actualizar 2_phi_delta_protocol.py
    Add Module 2 - The Capitán's Measure Φ-∆ Protocol ZL

--- 2_phi_delta_protocol.py
+++ 2_phi_delta_protocol.py
@@ -4,7 +4,7 @@ The Capitán's Measure — Φ-∆ Protocol ZL
 Author: Jaime Quilez Zamora & Leo Corazón
 Instituto de Física de la Información (IPI)
 Date: 2026-03-26
-License: CC BY 4.0
+License: Apache 2.0
 """
 
 # Simulación conceptual del Teorema Matriz en Python
